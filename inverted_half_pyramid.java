3b0d8a05cdc3bf7d170bd17f3633b871291c3c22
Humayra09 <83193225+Humayra09@users.noreply.github.com>
2022-12-23 15:10:53 +0530

    inverted half pyramid pattern

---
+++ inverted_half_pyramid.java
@@ -0,0 +1,23 @@
+/*   Pattern we need to print:
+            *****
+            ****
+            ***
+            **
+            *
+ */
+import java.util.Scanner;
+
+public class inverted_half_pyramid {
+    static Scanner sc = new Scanner(System.in);
+    public static void main(String args[]) {
+        System.out.print("Enter value of n:");
+        int n = sc.nextInt();
+        for(int i=n;i>=1;i--){
+            for(int j=1; j<=i ;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+    
+}
