44fb77d5f0d2a4529b8e2f657f41625b15c53383
Humayra09 <83193225+Humayra09@users.noreply.github.com>
2022-12-27 13:01:19 +0530

    number pyramid

---
+++ num_half_pyramid.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class num_half_pyramid {
+    static Scanner sc = new Scanner(System.in);
+    public static void main(String[] args){
+        System.out.print("Enter number of rows:");
+        int n = sc.nextInt();
+        int count = 1;
+        for(int i = 1; i<=n ; i++){
+            for(int j = 1 ; j<= i ; j++){
+                System.out.print(count+" ");
+                count++;
+            }
+            System.out.println();
+        }
+    }
+}
